a771f2e4ed080642360b549388ca9eb914c92773
bloody <novickiy.n.l@gmail.com>
2022-11-12 22:23:43 +0300

    Lesson1

--- 15/Program.cs
+++ 15/Program.cs
@@ -1,4 +1,28 @@
 ﻿// С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет (Вывести: NO)
 System.Console.Write("Ввод числа:");
 int a=Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine(a/100%10);
+int i=99,i1=999,i2=9999;
+
+if(a>i2)
+{
+  System.Console.WriteLine(a/100%10);
+}
+  else
+  {
+    if(a>i1)
+    {
+        System.Console.WriteLine(a/10%10);
+    }
+    else
+    {
+if(a>i)
+{
+    System.Console.WriteLine(a%10);
+}
+else
+{
+    System.Console.WriteLine("no");
+}
+    }
+  }
+
