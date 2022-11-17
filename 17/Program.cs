18fdfcace941a38d5557b99168dfe939e0f0fffd
bloody <novickiy.n.l@gmail.com>
2022-11-17 22:44:04 +0300

    Lesson 2

--- 17/Program.cs
+++ 17/Program.cs
@@ -1,15 +1,8 @@
 ﻿// 17. Дано число обозначающее день недели. Выяснить является номер дня недели выходным
 int day=5;
-//Monday - понедельник Tuesday - вторник Wednesday - среда Thursday - четверг Friday - пятница Saturday - суббота Sunday - воскресенье
-int mon=1;
-int tues=2;
-int wednes=3;
-int thurs=4;
-int fri=5;
 int satur=6;
 int sun=7;
-
-if(day!=6 || day!=7)
+if(day!=satur || day!=sun)
 {
     System.Console.WriteLine("Сегодня не выходной");
 }
--- 21/Program.cs
+++ 21/Program.cs
@@ -3,29 +3,29 @@ System.Console.WriteLine("Введите четверть");
 int a=Convert.ToInt32(Console.ReadLine());
 if(a==1)
 {
-System.Console.WriteLine("x=(0;+∞)");
-System.Console.WriteLine("y=(0;+∞)");
+System.Console.WriteLine("x=(0;+бесконечности)");
+System.Console.WriteLine("y=(0;+бесконечности)");
 }
 else
 {
     if(a==2)
     {
-        System.Console.WriteLine("x=(0;-∞)");
-        System.Console.WriteLine("y=(0;+∞)");
+        System.Console.WriteLine("x=(0;-бесконечности)");
+        System.Console.WriteLine("y=(0;+бесконечности)");
     }
       else
     {
         if(a==3)
         {
-            System.Console.WriteLine("x=(0;-∞)");
-            System.Console.WriteLine("y=(0;-∞)");
+            System.Console.WriteLine("x=(0;-бесконечности)");
+            System.Console.WriteLine("y=(0;-бесконечности)");
         }
         else
         {
             if(a==4)
             {
-                System.Console.WriteLine("x=(0;+∞)");
-                System.Console.WriteLine("y=(0;-∞)");
+                System.Console.WriteLine("x=(0;+бесконечности)");
+                System.Console.WriteLine("y=(0;-бесконечности)");
             }
             else
             {
