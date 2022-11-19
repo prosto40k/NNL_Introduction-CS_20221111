63ade8d48bd5b9215ff8c8e12155974690d6c66a
bloody <novickiy.n.l@gmail.com>
2022-11-19 10:34:49 +0300

    Lesson 2

---
+++ 16/Program.cs
@@ -0,0 +1,11 @@
+﻿//16. Дано число. Проверить кратно ли оно 7 и 23
+System.Console.WriteLine("Введите число");
+int a=Convert.ToInt32(Console.ReadLine());
+if (a%7==0 && a%23==0 )
+{
+    System.Console.WriteLine("Кратно");
+}
+else
+{
+    System.Console.WriteLine("Не кратно");
+}
---
+++ 18/Program.cs
@@ -0,0 +1,13 @@
+﻿//18. По двум заданным числам проверять является ли одно квадратом другого.
+System.Console.WriteLine("Число 1");
+int a=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Число 2");
+int b=Convert.ToInt32(Console.ReadLine());
+if (a/b==b || b/a==a)
+{
+    System.Console.WriteLine("Является");
+}     
+else
+{
+    System.Console.WriteLine("Не является");
+} 
