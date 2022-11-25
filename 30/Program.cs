aa592ef023c794ec4c67da17dc7d04409c218af5
bloody <novickiy.n.l@gmail.com>
2022-11-25 18:29:31 +0300

    Lesson 3

--- 30/Program.cs
+++ 30/Program.cs
@@ -3,7 +3,6 @@
 double sum=1;
 int i =2;
 int i1=1;
-double sqr;
 System.Console.WriteLine("Введите N");
 int N=Convert.ToInt32(Console.ReadLine());
 while (i1!=N) 
@@ -13,5 +12,4 @@ while (i1!=N)
     i1++;
 System.Console.WriteLine(sum);
 }
-sqr=sum;
 System.Console.WriteLine($"Произведение чисел от 1 до {N} = {sum}");
