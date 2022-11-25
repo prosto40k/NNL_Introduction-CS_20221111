43ead9bce511f883b53a8a32166df5e4c9685f74
bloody <novickiy.n.l@gmail.com>
2022-11-25 18:22:58 +0300

    Lesson 3

---
+++ 29/Program.cs
@@ -0,0 +1,13 @@
+﻿//29. Подсчитать сумму цифр в числе. Сделать подпрограмму.
+System.Console.WriteLine("Введите число");
+int N=Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int numb=0;
+while (N>1)
+{
+   numb=N%10+numb;
+   N=N/10;
+   i++; 
+}
+System.Console.WriteLine(numb);
+
