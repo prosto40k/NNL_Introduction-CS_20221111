6c11b4513ef0de887d98ff59453b555efb012218
bloody <novickiy.n.l@gmail.com>
2022-11-27 13:49:12 +0300

    Lesson 3

---
+++ 27.m/Program.cs
@@ -0,0 +1,22 @@
+﻿//27.  Возведите число А в натуральную степень B используя цикл
+int Sqr(int a, int b)
+{
+    int i = 0;
+    int sqr=0;
+while (i<1) 
+{
+    sqr=(int)Math.Pow(a,b);
+    i++;
+}
+return sqr;
+}
+System.Console.WriteLine("Введите число А");
+int a=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число В");
+int b=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Sqr(a,b));
+
+
+
+
+
---
+++ 30.m/Program.cs
@@ -0,0 +1,17 @@
+﻿//30. Написать программу вычисления произведения чисел от 1 до N
+double Sum(int N)
+{
+double sum=1;
+int i =2;
+int i1=1;
+while (i1!=N) 
+{
+   sum=sum*i;
+    i++;
+    i1++;
+}
+return sum;
+}
+System.Console.WriteLine("Введите N");
+int N=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Sum(N));
