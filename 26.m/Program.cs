9ed1a5901484d2c7d131b60ee8238c2d474bc10f
bloody <novickiy.n.l@gmail.com>
2022-11-27 13:28:45 +0300

    Lesson 3

---
+++ 26.m/Program.cs
@@ -0,0 +1,20 @@
+﻿//26. Найти сумму чисел от 1 до А
+double Sum(int N)
+{
+    double sum=0;
+    double x = 1;
+    int i =1;
+    while (i!=N) 
+{
+    i++;
+    x = x + 0.5;
+}
+System.Console.WriteLine(i);
+System.Console.WriteLine(x);
+sum = x * i;
+return sum;
+}
+System.Console.WriteLine("Введите N");
+int N=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Sum(N));
+
