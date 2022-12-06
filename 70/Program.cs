544b03d008f03b3e30b72ca280689cb02e89e7fe
bloody <novickiy.n.l@gmail.com>
2022-12-06 21:28:06 +0300

    Lesson 7

--- 70/Program.cs
+++ 70/Program.cs
@@ -1,11 +1,11 @@
 ﻿//70. Показать натуральные числа от 1 до N, N задано
 
-int Num(int n=5,int i)
+int Num(int n=4,int i=1)
         {
            if (n == i)
                 return i;
             else 
-                return Num(i+1);
+                return Num(i+i)
         }
 
-System.Console.WriteLine(Num());
+Num();
