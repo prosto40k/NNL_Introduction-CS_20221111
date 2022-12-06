b2ae69ab3fb8b3c5e5ab3c23c84ac2405293d7ee
bloody <novickiy.n.l@gmail.com>
2022-12-06 19:39:21 +0300

    Lesson 7

---
+++ 70/Program.cs
@@ -0,0 +1,11 @@
+﻿//70. Показать натуральные числа от 1 до N, N задано
+
+int Num(int n=5,int i)
+        {
+           if (n == i)
+                return i;
+            else 
+                return Num(i+1);
+        }
+
+System.Console.WriteLine(Num());
---
+++ 73/Program.cs
@@ -0,0 +1,11 @@
+﻿//73. Найти сумму элементов от M до N, N и M заданы
+
+int Sum(int m,int n)
+{
+    if (m==n) return m;
+    else
+     return Sum(m+1,n)+m;
+}
+
+System.Console.WriteLine(Sum(2,4));
+System.Console.WriteLine((2+4)*3/2);
