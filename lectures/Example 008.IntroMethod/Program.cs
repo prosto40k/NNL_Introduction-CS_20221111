8e5f4f74076af6f0fdb7e959afb2a2c49074a9b4
bloody <novickiy.n.l@gmail.com>
2023-01-05 16:10:42 +0300

    Lecture 2

---
+++ lectures/Example 008.IntroMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿//Максимальное из 9 чисел
+int Max(int arg1,int arg2,int arg3)
+{
+    int result=arg1;
+    if (arg2>result) result=arg2;
+    if (arg3>result) result=arg3;
+    return result;
+}
+
+int a1=11;
+int b1=2;
+int c1=3;
+
+int a2=4;
+int b2=5;
+int c2=6;
+
+int a3=7;
+int b3=8;
+int c3=9;
+
+int max=Max(
+    Max(a1,b1,c1),
+    Max(a2,b2,c2),
+    Max(a3,b3,c3));
+System.Console.WriteLine(max);
