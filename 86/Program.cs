9928b5d7ed8299f864d9c87a20abdce15cc854a4
bloody <novickiy.n.l@gmail.com>
2023-01-05 16:10:59 +0300

    Lesson 8

--- 86/Program.cs
+++ 86/Program.cs
@@ -1,9 +1,10 @@
 ﻿//86. Подсчитать сколько раз определенный символ (например 'a') встречается в строке.
 string a = "asdfga";
 int i = 0;
+char srch='a';
 foreach (char c in a)
 {
-    if (c=='a')
+    if (c==srch)
     {
         i++;
     }
