0a04c5083fa2a0b787a3f5ae2087309c5fafafb8
bloody <novickiy.n.l@gmail.com>
2023-01-06 03:05:50 +0300

    Lecture 3

---
+++ lectures/Example 012.Methods/Program.cs
@@ -0,0 +1,101 @@
+﻿// //1ая группа
+// void Method1()
+// {
+//     System.Console.WriteLine("Автор Я");
+// }
+// Method1();
+
+// //2ая группа
+// void Method2(string msg)
+// {
+//     System.Console.WriteLine(msg);
+// }
+// Method2(msg:"Текст сообщениая");
+// //
+// void Method21(string msg,int count)
+// {
+//     int i=1;
+//     while (i<count)
+//     {
+//         System.Console.WriteLine(msg);
+//         i++;
+//     }
+//     System.Console.WriteLine(msg);
+// }
+// Method21("Текст", 2);
+// //3 группа
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+
+// int year=Method3();
+// System.Console.WriteLine(year);
+
+// //4 группа
+
+// // string Method4(int count, string c )
+// // {
+// //     int i=0;
+// //     string result=String.Empty;
+// //     while (i<count)
+// //     {
+// //         result=result+c;
+// //         i++;
+// //     }
+// //     return result;
+// // }
+// string Method4(int count, string c )
+// {
+//     string result=String.Empty;
+//     for (int i = 0;i<count; i++)
+//     {
+//        result=result+c; 
+//     }
+//     return result;
+// }
+
+// string res=Method4(3, "as");
+// System.Console.WriteLine(res);
+
+// //цикл в цикле
+// //Таблица умножения
+// int n=2;
+// for (int i = 2; i < 10; i++)
+// {
+//     System.Console.WriteLine($"Для {n}");
+//     for (int j = 2; j < 10; j++)
+//     {
+//         System.Console.WriteLine($"{i} * {j} = {i*j}");
+//     }
+//     n++;
+//     System.Console.WriteLine();
+// }
+// //Замена символа в строке
+// string text=" Qs1 1 F  ФЫАфыа ываыв-кС сыф 234";
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result=String.Empty;
+//     int length=text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if (text[i]==oldValue) result=result+$"{newValue}";
+//         else
+//         {
+//             result=result+ $"{text[i]}";
+//         }
+            
+//     }
+//     return result;
+
+// }
+// System.Console.WriteLine(text);
+// string newText=Replace(text, ' ', '|');
+// // System.Console.WriteLine(newText);
+// newText=Replace(newText, 'к', 'К');
+// // System.Console.WriteLine(newText);
+// newText=Replace(newText, 'С', 'с');
+// System.Console.WriteLine(newText);
+
+
---
+++ lectures/Example 013.MethodsContinue/Program.cs
@@ -0,0 +1,37 @@
+﻿//Метод от меньшего к большему в массиве
+
+int[] arr={1,2,1,3,7,5};
+
+void Print(int[] col)
+{
+    int count=col.Length;
+    int position=0;
+    while (position<count)
+    {
+        System.Console.Write($"[{position}]");//вывод индекса элемента массива
+        System.Console.Write($"[{col[position]}]\n");
+        position++;
+    }
+}
+
+void SelectionSort(int[] array)
+{
+    
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        int minPosition=i;
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if (array[j]<array[minPosition]) minPosition=j;
+        }
+        int temporary=array[i];
+        array[i]=array[minPosition];
+        array[minPosition]=temporary;
+    }
+    
+}
+
+Print(arr);
+SelectionSort(arr);
+System.Console.WriteLine();
+Print(arr);
